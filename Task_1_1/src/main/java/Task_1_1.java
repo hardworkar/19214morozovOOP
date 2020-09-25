2ef3a3f8a7737e8221b5f1b7795e045733b6f81b
hardworkar <arsmoroz@ya.ru>
2020-09-25 13:18:27 +0700

    Task_1_1 правки

--- Task_1_1/src/main/java/Task_1_1.java
+++ Task_1_1/src/main/java/Task_1_1.java
@@ -1,15 +1,69 @@
 import java.util.Arrays;
 
 public class Task_1_1 {
+
     public static void main(String[] args){
         int[] array = new int[] {10,2,1,3,0};
         System.out.println(Arrays.toString(sort(array)));
     }
-    /*
-     * simple sort function, returns sorted array
-     * */
+    // simple sort
     public static int[] sort(int[] arr){
-        Heap heap = new Heap(arr);
-        return heap.sort();
+        return Heap.sort(arr);
     }
+
+    private static class Heap {
+        private final int[] array;
+        private int heapSize;
+        Heap(int[] arr){
+            heapSize = 0;
+            array = new int[arr.length];
+            for (int j : arr) {
+                array[heapSize++] = j;
+                siftUp(heapSize - 1);
+            }
+        }
+        private void siftUp(int idx) {
+            if(array[idx] < array[(idx-1) / 2]){
+                int c = array[idx];
+                array[idx] = array[(idx-1) / 2];
+                array[(idx-1) / 2] = c;
+                siftUp((idx-1) / 2);
+            }
+        }
+        private void siftDown(int idx) {
+            if(2 * idx + 1 >= heapSize) {
+                return;
+            }
+            int left = 2 * idx + 1;
+            int right = 2 * idx + 2;
+            int j = left;
+            if(right < heapSize && array[right] < array[left]) {
+                j = right;
+            }
+            if(array[idx] <= array[j]) {
+                return;
+            }
+            int c = array[idx];
+            array[idx] = array[j];
+            array[j] = c;
+            siftDown(j);
+        }
+        private int extractMin() {
+            int min = array[0];
+            array[0] = array[heapSize-1];
+            heapSize--;
+            siftDown(0);
+            return min;
+        }
+        private static int[] sort(int[] arr) {
+            Heap heap = new Heap(arr);
+            int[] res = new int[heap.heapSize];
+            int i = 0;
+            while(heap.heapSize > 0) {
+                res[i++] = heap.extractMin();
+            }
+            return res;
+        }
+    }
+
 }
--- Task_1_1/src/test/java/Task_1_1Test.java
+++ Task_1_1/src/test/java/Task_1_1Test.java
@@ -5,7 +5,9 @@ public class Task_1_1Test {
 
     @Test
     public void EasyArrays() {
-        Assert.assertArrayEquals(new int[]{1, 2, 3, 4, 5}, Task_1_1.sort(new int[]{5, 4, 3, 2, 1}));
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        int[] sortedArr = Task_1_1.sort(arr);
+        Assert.assertArrayEquals(arr, sortedArr);
         Assert.assertArrayEquals(new int[]{777}, Task_1_1.sort(new int[]{777}));
         Assert.assertArrayEquals(new int[]{}, Task_1_1.sort(new int[]{}));
         Assert.assertArrayEquals(new int[]{33, 44}, Task_1_1.sort(new int[]{44, 33}));
